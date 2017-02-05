d2fbd244af846198107158267ee48166f9319312
VannaM <vmavromatis18@gmail.com>
2017-02-05 13:00:45 -0500

    edit

--- pic.py
+++ pic.py
@@ -10,33 +10,40 @@ picFile.write(header)
 
 def whoKnows():
 
-    numRows = 2
-    numCols = 2
+    numRows = 3
+    numCols = 3
     rowLen = xRes/numRows
     colLen = yRes/numCols
 
     colors = []
-    for i in range(numRows + numCols):
-        r = random.randrange(255)
-        g = random.randrange(255)
-        b = random.randrange(255)
-        colors.append([r,g,b])
+    for i in range(numRows):
+        thisRow = []
+        for j in range (numCols):
+            r = random.randrange(255)
+            g = random.randrange(255)
+            b = random.randrange(255)
+            thisRow.append([r,g,b])
+        colors.append(thisRow)
 
     for i in range(xRes):
 
         if (i % rowLen == 0):
-            rowNum = i/rowLen
+            rowNum = i/rowLen - 1
 
-        color = colors[rowNum]
+        colorRow = colors[rowNum]
+        color = colorRow[0]
 
         for j in range(yRes):
             r = color[0]
             g = color[1]
             b = color[2]
-
-            if (j%colLen == 0 and j > 0):
-                color = colors[rowNum + numRows]
-                picFile.write('0 0 0 ')
+            if (j%colLen == 0):
+                try:
+                    color = colorRow[j/colLen - 1]
+                    picFile.write('0 0 0 ')
+                except:
+                    print j
+                    return
             elif (i % rowLen == 0 or i == 0 or j == 0 or i == xRes-1 or j == yRes-1):
                 picFile.write('0 0 0 ')
             else:
